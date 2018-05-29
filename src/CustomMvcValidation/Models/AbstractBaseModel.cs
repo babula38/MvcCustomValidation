d9173b1c0199fa49af389dc8bc4e9beb05808e61
babula38 <babula38@gmail.com>
2018-05-29 10:41:34 +0530

    adding basic files

---
+++ src/CustomMvcValidation/Models/AbstractBaseModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CustomMvcValidation.Models
+{
+    public abstract class AbstractBaseModel : IValidatableObject
+    {
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
